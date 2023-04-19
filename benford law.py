242015687d829bd43661ca3e605187b0d1465127
avyprem <130694108+avyprem@users.noreply.github.com>
2023-04-19 13:12:00 -0400

    Update benford law.py

--- benford law.py
+++ benford law.py
@@ -1,1 +1,29 @@
-print("Hello World")
+import csv
+
+
+def printMenu():
+        print (input("Read sales data enter 1 "))
+
+readsalesdata = "1"
+userInput = "" 
+
+def analyzesales():
+        print("analyzing")
+        with open("sales - sales.csv") as file:
+                reader = csv.reader(file, delimiter=",")
+                list = list(reader)
+
+                
+
+                
+userInput = ""
+enterCustomerOption = "1"
+generateCustomerOption = "2"
+exitCondition = "9"
+
+
+while userInput != exitCondition:
+    printMenu()
+    userInput = input();
+    if userInput == readsalesdata:
+                analyzesales()
